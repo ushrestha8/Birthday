b29a24b2ee844c2e3a3a39f7d762123759e1ad4a
Utsav <shresthautsab345@gmail.com>
2025-12-07 02:37:54 -0600

    Build for github page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Birthday/",   // MUST match your GitHub repo name
+  build: {
+    outDir: "docs",      // <-- build into /docs for GitHub Pages
+  },
+});
